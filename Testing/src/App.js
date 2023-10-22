0eb6b2831a1140a7e73506c264e6d11f60a266a9
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-10-22 21:37:24 +0100

    Passing JSX in a children prop

--- Testing/src/App.js
+++ Testing/src/App.js
@@ -1,36 +1,54 @@
 import React from 'react';
-import { getImageUrl } from './utils.js';
+// import { getImageUrl } from './utils.js';
 import PropTypes from 'prop-types';
-
-function Avatar({ person, size }) {
-    let resolution = 'b'
-    if (size < 90) {
-        resolution = 's'
-    }
-    return (
-        <img
-            className="avatar"
-            src={getImageUrl(person.imageId, resolution)}
-            alt={person.name}
-            width={size}
-            height={size}
-        />
-    );
-}
-
-Avatar.propTypes = {
-    person: PropTypes.object,
-    size: PropTypes.number,
-}
 
 export default function Profile() {
     return (
-        <Avatar
-            size={90}
-            person={{
-                name: 'Gregorio Y. Zara',
-                imageId: '7vQD0fP'
-            }}
-        />
+        <div>
+            <Card>
+                <Photo />
+            </Card>
+            <Card>
+                <About />
+            </Card>
+        </div>
     );
+}
+
+function Card({ children }) {
+    return (
+        <div className="card">
+            <div className="card-content">
+                {children}
+            </div>
+        </div>
+    )
+}
+
+Card.propTypes = {
+    children: PropTypes.object
+}
+
+
+function Photo() {
+    return (
+        <>
+            <h1>Photo</h1>
+            <img
+                className="avatar"
+                src="https://i.imgur.com/OKS67lhm.jpg"
+                alt="Aklilu Lemma"
+                width={70}
+                height={70}
+            />
+        </>
+    )
+}
+function About() {
+    return (
+        <>
+            <h1>About</h1>
+            <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
+        </>
+    )
 }
