df0a6907b807600f177b55a777c68ccae2263c03
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-10-22 21:14:43 +0100

    convert html into jsk and pass props

--- Testing/src/App.js
+++ Testing/src/App.js
@@ -1,58 +1,67 @@
 import React from 'react';
 import { getImageUrl } from './utils.js';
+import PropTypes from 'prop-types';
 
 export default function Gallery() {
     return (
         <div>
             <h1>Notable Scientists</h1>
-            <section className="profile">
-                <h2>Maria Skłodowska-Curie</h2>
-                <img
-                    className="avatar"
-                    src={getImageUrl('szV5sdG')}
-                    alt="Maria Skłodowska-Curie"
-                    width={70}
-                    height={70}
-                />
-                <ul>
-                    <li>
-                        <b>Profession: </b>
-                        physicist and chemist
-                    </li>
-                    <li>
-                        <b>Awards: 4 </b>
-                        (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-                    </li>
-                    <li>
-                        <b>Discovered: </b>
-                        polonium (chemical element)
-                    </li>
-                </ul>
-            </section>
-            <section className="profile">
-                <h2>Katsuko Saruhashi</h2>
-                <img
-                    className="avatar"
-                    src={getImageUrl('YfeOqp2')}
-                    alt="Katsuko Saruhashi"
-                    width={70}
-                    height={70}
-                />
-                <ul>
-                    <li>
-                        <b>Profession: </b>
-                        geochemist
-                    </li>
-                    <li>
-                        <b>Awards: 2 </b>
-                        (Miyake Prize for geochemistry, Tanaka Prize)
-                    </li>
-                    <li>
-                        <b>Discovered: </b>
-                        a method for measuring carbon dioxide in seawater
-                    </li>
-                </ul>
-            </section>
+            <Profile name={"Maria Skłodowska-Curie"}
+                imageId={"szV5sdG"}
+                profession={"physicist and chemist"}
+                awards={["Nobel Prize in Physics", " Nobel Prize in Chemistry", "Davy Medal", "Matteucci Medal"]}
+                discoveries={"polonium (chemical element)"} />
+
+            <Profile
+                name={"Katsuko Saruhashi"}
+                imageId={"YfeOqp2"}
+                profession={"geochemist"}
+                awards={["Miyake Prize for geochemistry", "Tanaka Prize"]}
+                discoveries={"a method for measuring carbon dioxide in seawater"}
+            />
+
         </div>
     );
+}
+
+function Profile({ name, imageId, profession, awards, discoveries }) {
+    return (
+        <section className="profile">
+            <h2>{name}</h2>
+            <img
+                className="avatar"
+                src={getImageUrl(imageId)}
+                alt="Maria Skłodowska-Curie"
+                width={70}
+                height={70}
+            />
+            <ul>
+                <li>
+                    <b>Profession: </b>
+                    {profession}
+                </li>
+                <li>
+                    <b>Awards: {awards.length} </b>
+                    ({
+                        awards.map(award => {
+                            return award + ', '
+                        })
+                    })
+                </li>
+                <li>
+                    <b>Discovered: </b>
+                    {discoveries}
+                </li>
+            </ul>
+        </section>
+    )
+}
+
+Profile.propTypes = {
+    name: PropTypes.string,
+    imageId: PropTypes.string,
+    profession: PropTypes.string,
+    awards: PropTypes.array,
+    discoveries: PropTypes.string,
+
 }
