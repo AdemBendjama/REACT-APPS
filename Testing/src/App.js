c19336ee2342886827ba42d0c80a277393d83d1f
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-10-22 21:20:32 +0100

    solve react challenge

--- Testing/src/App.js
+++ Testing/src/App.js
@@ -2,66 +2,35 @@ import React from 'react';
 import { getImageUrl } from './utils.js';
 import PropTypes from 'prop-types';
 
-export default function Gallery() {
+function Avatar({ person, size }) {
+    let resolution = 'b'
+    if (size < 90) {
+        resolution = 's'
+    }
     return (
-        <div>
-            <h1>Notable Scientists</h1>
-            <Profile name={"Maria Skłodowska-Curie"}
-                imageId={"szV5sdG"}
-                profession={"physicist and chemist"}
-                awards={["Nobel Prize in Physics", " Nobel Prize in Chemistry", "Davy Medal", "Matteucci Medal"]}
-                discoveries={"polonium (chemical element)"} />
-
-            <Profile
-                name={"Katsuko Saruhashi"}
-                imageId={"YfeOqp2"}
-                profession={"geochemist"}
-                awards={["Miyake Prize for geochemistry", "Tanaka Prize"]}
-                discoveries={"a method for measuring carbon dioxide in seawater"}
-            />
-
-        </div>
+        <img
+            className="avatar"
+            src={getImageUrl(person.imageId, resolution)}
+            alt={person.name}
+            width={size}
+            height={size}
+        />
     );
 }
 
-function Profile({ name, imageId, profession, awards, discoveries }) {
-    return (
-        <section className="profile">
-            <h2>{name}</h2>
-            <img
-                className="avatar"
-                src={getImageUrl(imageId)}
-                alt="Maria Skłodowska-Curie"
-                width={70}
-                height={70}
-            />
-            <ul>
-                <li>
-                    <b>Profession: </b>
-                    {profession}
-                </li>
-                <li>
-                    <b>Awards: {awards.length} </b>
-                    ({
-                        awards.map(award => {
-                            return award + ', '
-                        })
-                    })
-                </li>
-                <li>
-                    <b>Discovered: </b>
-                    {discoveries}
-                </li>
-            </ul>
-        </section>
-    )
+Avatar.propTypes = {
+    person: PropTypes.object,
+    size: PropTypes.number,
 }
 
-Profile.propTypes = {
-    name: PropTypes.string,
-    imageId: PropTypes.string,
-    profession: PropTypes.string,
-    awards: PropTypes.array,
-    discoveries: PropTypes.string,
-
+export default function Profile() {
+    return (
+        <Avatar
+            size={90}
+            person={{
+                name: 'Gregorio Y. Zara',
+                imageId: '7vQD0fP'
+            }}
+        />
+    );
 }
