421c672b517c692a8e44044f309e6c4295bb63d2
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-10-28 15:21:47 +0100

    solve react exo

--- Testing/src/App.js
+++ Testing/src/App.js
@@ -1,54 +1,51 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
+import { letters } from './data.js';
+import Letter from './Letter.js';
 
-// editing
-// saving
-export default function App() {
+export default function MailClient() {
+    const [selectedIds, setSelectedId] = useState([]);
 
-    const [editing, setEditing] = useState(false)
-    const [form, setForm] = useState({
-        firstName: 'Jane',
-        lastName: 'Jacobs'
-    })
-
-    function handleInputChange(e) {
-        let field = e.target.id === "firstNameInput" ? 'firstName' : 'lastName';
-        setForm(
-            {
-                ...form,
-                [field]: e.target.value
-            }
-        )
+    function handleToggle(toggledId) {
+        // TODO: allow multiple selection
+        let found = selectedIds.filter(id => id === toggledId).length !== 0;
+        let newSelectedIds
+        if (found) {
+            newSelectedIds = selectedIds.map(id => {
+                if (id === toggledId) {
+                    return null
+                } else {
+                    return id
+                }
+            }).filter(id => id !== null)
+        } else {
+            newSelectedIds = [...selectedIds, toggledId]
+        }
+        setSelectedId(newSelectedIds)
     }
-
-    function handleEdit(e) {
-        e.preventDefault()
-        setEditing(!editing)
-    }
-
-    let display = { display: 'none' }
-
-    if (editing) {
-        display = { display: '' };
-    }
-
 
     return (
-        <form id="form">
-            <label>
-                First name:
-                <b id="firstNameText">{!editing ? form.firstName : ""}</b>
-                <input id="firstNameInput" defaultValue={form.firstName} style={display}
-                    onChange={handleInputChange} />
-            </label>
-            <label>
-                Last name:
-                <b id="lastNameText">{!editing ? form.lastName : ""}</b>
-                <input id="lastNameInput" defaultValue={form.lastName} style={display}
-                    onChange={handleInputChange} />
-            </label>
-            <button type="submit" id="editButton" onClick={handleEdit}>{!editing ? "Edit Profile" : "Save Profile"}</button>
-            <p><i id="helloText">Hello, {form.firstName + " " + form.lastName}!</i></p>
-        </form>
-    )
+        <>
+            <h2>Inbox</h2>
+            <ul>
+                {letters.map(letter => (
+                    <Letter
+                        key={letter.id}
+                        letter={letter}
+                        isSelected={
+                            // TODO: allow multiple selection
+                            selectedIds.some(id => letter.id === id)
+                        }
+                        onToggle={handleToggle}
+                    />
+                ))}
+                <hr />
+                <p>
+                    <b>
+                        You selected {selectedIds.length} letters
+                    </b>
+                </p>
+            </ul>
+        </>
+    );
 }
