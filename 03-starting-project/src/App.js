48f3e750ac6f159cd483d65ae48a2962f7c2e729
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-12-02 11:33:59 +0100

    extract components

--- 03-starting-project/src/App.js
+++ 03-starting-project/src/App.js
@@ -1,16 +1,20 @@
-import logo from './assets/investment-calculator-logo.png';
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Table from "./components/Table";
 
 function App() {
+
   const calculateHandler = (userInput) => {
     // Should be triggered when form is submitted
     // You might not directly want to bind it to the submit event on the form though...
+    userInput.preventDefault()
 
     const yearlyData = []; // per-year results
 
-    let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
-    const expectedReturn = +userInput['expected-return'] / 100;
-    const duration = +userInput['duration'];
+    let currentSavings = parseFloat(userInput.target['current-savings'].value); // feel free to change the shape of this input object!
+    const yearlyContribution = parseFloat(userInput.target['yearly-contribution'].value); // as mentioned: feel free to change the shape...
+    const expectedReturn = parseFloat(userInput.target['expected-return'].value) / 100;
+    const duration = parseFloat(userInput.target['duration'].value);
 
     // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
@@ -25,72 +29,21 @@ function App() {
       });
     }
 
+
     // do something with yearlyData ...
   };
 
   return (
     <div>
-      <header className="header">
-        <img src={logo} alt="logo" />
-        <h1>Investment Calculator</h1>
-      </header>
+      <Header></Header>
 
-      <form className="form">
-        <div className="input-group">
-          <p>
-            <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
-          </p>
-          <p>
-            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="expected-return">
-              Expected Interest (%, per year)
-            </label>
-            <input type="number" id="expected-return" />
-          </p>
-          <p>
-            <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
-          </p>
-        </div>
-        <p className="actions">
-          <button type="reset" className="buttonAlt">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Calculate
-          </button>
-        </p>
-      </form>
+      <Form onSubmit={calculateHandler} ></Form>
 
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
-      <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
+      <Table></Table>
+
     </div>
   );
 }
