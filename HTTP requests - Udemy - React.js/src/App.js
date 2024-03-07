eda81944917c7aee5ecf39a06986684025470383
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-03-07 19:14:57 +0100

    send get request and display movies

--- HTTP requests - Udemy - React.js/src/App.js
+++ HTTP requests - Udemy - React.js/src/App.js
@@ -1,49 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: 'Some Dummy Movie',
-  //     openingText: 'This is the opening text of the movie',
-  //     releaseDate: '2021-05-18',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Some Dummy Movie 2',
-  //     openingText: 'This is the second opening text of the movie',
-  //     releaseDate: '2021-05-19',
-  //   },
-  // ];
 
   const [movies, setMovies] = useState([])
 
+  const fetchMovies = async () => {
 
-  async function fetchMovies() {
-    const response = await fetch('https://www.swapi.tech/api/films')
+    try {
+      const response = await fetch('https://www.swapi.tech/api/films')
 
-    const data = await response.json()
-
-    const convertedData = data.result.map((movie) => {
-      return {
-        id: movie._id,
-        title: movie.properties.title,
-        openingText: movie.properties.opening_crawl,
-        releaseDate: movie.properties.release_date,
+      if (!response.ok) {
+        throw new Error('Failed to fetch movie data')
       }
-    })
 
-    setMovies(convertedData);
+      const data = await response.json()
+
+      const convertedData = data.result.map((movie) => {
+        return {
+          id: movie._id,
+          title: movie.properties.title,
+          openingText: movie.properties.opening_crawl,
+          releaseDate: movie.properties.release_date,
+        }
+      })
+
+      setMovies(convertedData);
+
+    } catch (error) {
+      console.error('Error', error);
+    }
 
   }
+
+  useEffect(() => {
+    fetchMovies()
+  }, [])
+
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMovies}>Fetch Movies</button>
+        <button >Fetch Movies</button>
       </section>
       <section>
         <MoviesList movies={movies} />
