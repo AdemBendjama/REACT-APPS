cfa25237e08a47c980d4fb6bf9dcab99d277b741
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-03-11 14:27:37 +0100

    create user friendly input element

--- Forms - Udemy - React.js/src/components/SimpleInput.js
+++ Forms - Udemy - React.js/src/components/SimpleInput.js
@@ -1,12 +1,38 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+
+  const [isTouched, setIsTouched] = useState(false)
+  const [enteredValue, setEnteredValue] = useState('')
+  const isValid = isTouched && enteredValue.trim().length > 0
+  const inputStyle = (isTouched && !isValid) ? 'form-control invalid' : 'form-control'
+  const errorText = <p className="error-text">Name Can't Be Empty</p>
+
+  const nameChangeHandler = (event) => {
+    setEnteredValue(event.target.value)
+  }
+
+  const nameBlurHandler = (event) => {
+    setIsTouched(true)
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    if (isValid) alert('Submited !')
+
+    setEnteredValue('')
+    setIsTouched(false)
+  }
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={submitHandler}>
+      <div div className={inputStyle} >
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
+        <input type='text' id='name' onChange={nameChangeHandler} onBlur={nameBlurHandler} value={enteredValue} />
+        {isTouched && !isValid && errorText}
+      </div >
       <div className="form-actions">
-        <button>Submit</button>
+        <button type="submit" disabled={!isValid} className={!isValid && "disabled"}>Submit</button>
       </div>
     </form>
   );
