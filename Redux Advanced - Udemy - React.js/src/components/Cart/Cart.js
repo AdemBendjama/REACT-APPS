b20b2097e6b7a348750689dbed2f8af4fa41d84e
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-03-14 00:22:26 +0100

    Add ability to see cart items quantity from cart Button, Add msg when cart is Empty

--- Redux Advanced - Udemy - React.js/src/components/Cart/Cart.js
+++ Redux Advanced - Udemy - React.js/src/components/Cart/Cart.js
@@ -5,12 +5,16 @@ import CartItem from './CartItem';
 
 const Cart = (props) => {
   const cartItems = useSelector(state => state.cart.cartItems)
+  const isEmpty = cartItems.length === 0
 
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        {
+        {isEmpty &&
+          <p>Your cart is currently empty.</p>
+        }
+        {!isEmpty &&
           cartItems.map(item => (
             <CartItem
               item={{ id: item.id, title: item.title, quantity: item.quantity, price: item.price }}
--- Redux Advanced - Udemy - React.js/src/components/Cart/CartButton.js
+++ Redux Advanced - Udemy - React.js/src/components/Cart/CartButton.js
@@ -1,9 +1,12 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { cartActions } from '../../store/cart';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
   const dispatch = useDispatch()
+  const cartItems = useSelector(state => state.cart.cartItems)
+
+  const totalQuantity = cartItems.reduce(((sum, item) => sum + item.quantity), 0)
 
   const toggleCartHandler = () => {
     dispatch(cartActions.toggleCart())
@@ -12,7 +15,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{totalQuantity}</span>
     </button>
   );
 };
