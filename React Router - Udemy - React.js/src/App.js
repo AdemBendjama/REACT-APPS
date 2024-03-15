b284fc494b7c10bd312f76bd564c3531453c5c61
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-03-15 22:29:09 +0100

    create initial app with react router

---
+++ React Router - Udemy - React.js/src/App.js
@@ -0,0 +1,12 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter(
+  [
+    { path: '', element: <h1>Hello Latvia</h1> }
+  ]
+)
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+export default App;
