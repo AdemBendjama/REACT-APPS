4ed3b6a2c68ed0006d2fe5b86d6349c10e0dffaa
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-03-17 23:38:21 +0100

    create multiple routes with 2 diffrent approaches

--- React Router - Udemy - React.js/src/App.js
+++ React Router - Udemy - React.js/src/App.js
@@ -1,10 +1,27 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from './components/HomePage'
+import ProductsPage from './components/ProductsPage'
+
+// Alternative way to define routes with Route
+// import { Route, createRoutesFromElements } from "react-router-dom";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+
+//     <Route path="/" element={<HomePage />}></Route>
+//     <Route path="products" element={<ProductsPage />}></Route>
+//   </Route>
+// )
+
+// const router = createBrowserRouter(routeDefinitions)
 
 const router = createBrowserRouter(
   [
-    { path: '', element: <h1>Hello Latvia</h1> }
+    { path: '/', element: <HomePage /> },
+    { path: 'products', element: <ProductsPage /> }
   ]
 )
+
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ React Router - Udemy - React.js/src/components/ProductsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductsPage() {
+    return (
+        <div>
+            Products List
+        </div>
+    )
+}
+
+export default ProductsPage
