4b8be87784a9fae63795ef1c2e7c38b9640c2d60
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-03-25 09:57:45 +0100

    Work with getStaticProps/getStaticPaths/getServerSideProps and learn thier usecases

--- Next.js Practice Project - Udemy - React.js/pages/[meetupId]/index.js
+++ Next.js Practice Project - Udemy - React.js/pages/[meetupId]/index.js
@@ -3,10 +3,46 @@ import { useRouter } from 'next/router'
 import React from 'react'
 
 function MeetupDetailPage(props) {
-    const router = useRouter()
-    const meetupId = router.query.meetupId
-    // fetch item from backend with the id
-    // dummy object for testing
+    // const router = useRouter()
+    // const meetupId = router.query.meetupId
+    // // fetch item from backend with the id
+    // // dummy object for testing
+    // const meetup = {
+    //     id: meetupId,
+    //     title: 'A First Meetup',
+    //     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    //     address: 'Some address 5, 12345 Some City',
+    //     description: 'This is a first meetup!'
+    // }
+    const meetup = props.meetup
+
+    return (
+        <MeetupDetail {...meetup} />
+    )
+}
+
+export function getStaticPaths() {
+    // fallback false means all listed params are all the possible values for the dnamic url so anything outside of it throws a 404 error
+    // fallback true means the listed params are some and any new params would result in getStaticProps to excute
+    return ({
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1'
+                }
+            },
+            {
+                params: {
+                    meetupId: 'm1'
+                }
+            }]
+    })
+}
+
+export async function getStaticProps(context) {
+    const meetupId = context.params.meetupId
+
     const meetup = {
         id: meetupId,
         title: 'A First Meetup',
@@ -15,9 +51,11 @@ function MeetupDetailPage(props) {
         description: 'This is a first meetup!'
     }
 
-    return (
-        <MeetupDetail {...meetup} />
-    )
+    return ({
+        props: {
+            meetup: meetup
+        }
+    })
 }
 
 export default MeetupDetailPage
--- Next.js Practice Project - Udemy - React.js/pages/index.js
+++ Next.js Practice Project - Udemy - React.js/pages/index.js
@@ -18,10 +18,52 @@ const DUMMY_DATA = [
     }
 ]
 
-function HomePage() {
+function HomePage(props) {
     return (
-        <MeetupList meetups={DUMMY_DATA} />
+        <MeetupList meetups={props.meetups} />
     )
 }
+
+// build in reserved next js function 
+// function excutes on server side
+// excutes once during build process
+// excutes again depending on revalidate prop timer
+/*
+* can fetch data before serving the page to the user by excuting in server
+* and then passing any data through props, can only be written inside pages components,
+* in summery allows pre rendering pages in server with the fetched data
+* because in a normal situation the page would be served empty without data then it would be filled with data after fetching in the second re-render 
+*/
+// ! this form of Pre-rendering is called Static generation
+export async function getStaticProps() {
+    // Fetch data here
+    //...
+
+    // pass data into page with components
+    return (
+        {
+            props: {
+                meetups: DUMMY_DATA
+            },
+            revalidate: 10
+        }
+    )
+}
+
+// use it when ur data and ui changes frequently 
+// and when you need to use request and response headers in ur data menupilation
+// ! this form of Pre-rendering is called Server side rendering 
+// export async function getServerSideProps(context) {
+//     const request = context.req
+//     const response = context.res
+
+//     return (
+//         {
+//             props: {
+//                 meetups: DUMMY_DATA
+//             }
+//         }
+//     )
+// }
 
 export default HomePage
